f0610456caf732a9b81de7d5e762da5e6fffe750
Austin Hernandez <ahernandez1170@gmail.com>
2020-12-23 19:25:12 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,8 @@
+"../lib/introduction.rb"
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with _language(Dan, Ember.js)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
